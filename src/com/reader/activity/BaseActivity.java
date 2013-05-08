18dc767d78ce75a75a92922a549b73d4d8409d42
tsang gary <ws_wishao@163.com>
2013-05-09 01:29:54 +0800

    button test

--- src/com/reader/activity/BaseActivity.java
+++ src/com/reader/activity/BaseActivity.java
@@ -5,13 +5,27 @@ import com.reader.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BaseActivity extends Activity {
-
+	Button testUpdateButton = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_base);
+		
+		testUpdateButton = (Button)findViewById(R.id.button1);
+		OnClickListener clickListener = new OnClickListener(){
+				public void onClick(View v) {
+					String title = "";
+					Toast.makeText(BaseActivity.this,title,Toast.LENGTH_SHORT).show();
+				}
+	        };
+	    testUpdateButton.setOnClickListener(clickListener);
+		
 	}
 
 	@Override
