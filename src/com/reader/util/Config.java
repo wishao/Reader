e2028f2ec40f8d86de5a901c7beb80cdca7ce742
tsang gary <ws_wishao@163.com>
2013-05-13 21:31:57 +0800

    a

--- src/com/reader/util/Config.java
+++ src/com/reader/util/Config.java
@@ -1,7 +1,7 @@
 package com.reader.util;
 
 public class Config {
-	public static final String HTTP_IP = "192.168.0.179";
+	public static final String HTTP_IP = "192.168.1.102";
 	public static final String HTTP_PATH = "http://" + HTTP_IP
 			+ ":8080/LBS-Reader/";
 	public static final String HTTP_LOGIN = HTTP_PATH + "user!login";
