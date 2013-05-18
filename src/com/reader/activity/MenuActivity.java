de65dd0b202f6f6b053274dec5b4c8692b31d5df
tsang gary <ws_wishao@163.com>
2013-05-18 10:14:13 +0800

    color

--- src/com/reader/activity/MenuActivity.java
+++ src/com/reader/activity/MenuActivity.java
@@ -14,7 +14,6 @@ import android.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MenuActivity extends Activity {
 
